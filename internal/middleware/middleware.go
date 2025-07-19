d1a5913cb1a66493b610d5325efb3d7d94cbaca5
احمد عصام الدين علي الخرجاوي <ae3411102@gmail.com>
2025-07-19 23:22:13 +0300

    Timeout function

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -1,6 +1,10 @@
 package middleware
 
 import (
+	"context"
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 )
@@ -13,7 +17,22 @@ func RequestLogger() gin.HandlerFunc {
 }
 
 func Timeout() gin.HandlerFunc {
-	// 	return func (c *gin.Context){
-	// 		var timeout = context.WithTimeout(c.Request.Context(),2*time.Second)
-	// 	}
+	return func(c *gin.Context) {
+		context, cancel := context.WithTimeout(c.Request.Context(), 2*time.Second)
+		defer cancel()
+		c.Request = c.Request.WithContext(context)
+		done := make(chan bool)
+		go func() {
+			c.Next()
+			done <- true
+		}()
+		select {
+		case <-done:
+			return
+		case <-context.Done():
+			c.JSON(http.StatusGatewayTimeout, gin.H{"error": "Request Timeout"})
+			c.Abort()
+		}
+
+	}
 }
